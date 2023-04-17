4af159334c15e3bacb086faca27d7317b296825f
orhun <akmilorhun@gmail.com>
2023-04-17 20:50:06 +0300

    d

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from flask import Flask, request, jsonify
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import json
 import re
+import html5lib
 
 app = Flask(__name__)
 
 
-@app.route('/getnewurl', methods=['GET'])
-def getnewurl():
+@app.route('/getnewurl_usa', methods=['GET'])
+def getnewurl_usa():
     return 'https://www.investing.com/news/cryptocurrency-news'
+
+@app.route('/getnewurl_tr', methods=['GET'])
+def getnewurl_tr():
+    return 'https://tr.investing.com/news/cryptocurrency-news'
+
+@app.route('/getnewurl_it', methods=['GET'])
+def getnewurl_it():
+    return 'https://it.investing.com/news/cryptocurrency-news'
+
+@app.route('/getnewurl_es', methods=['GET'])
+def getnewurl_es():
+    return 'https://es.investing.com/news/cryptocurrency-news'
+
+@app.route('/getnewurl_cn', methods=['GET'])
+def getnewurl_cn():
+    return 'https://cn.investing.com/news/cryptocurrency-news'
+
+@app.route('/getnewurl_fr', methods=['GET'])
+def getnewurl_fr():
+    return 'https://fr.investing.com/news/cryptocurrency-news'
 
 @app.route('/parse-news', methods=['POST'])
 def parse_news():
     html_code = request.get_data(as_text=True)
     html_code = re.sub(r'\\', '', html_code)  # \ karakterlerini sil
-    soup = BeautifulSoup(html_code, 'html.parser')
+    soup = BeautifulSoup(html_code, 'html5lib')
 
     titles = soup.select('.largeTitle a.title')
     dates = soup.select('.largeTitle .date')
@@ -23,20 +44,16 @@ def parse_news():
 
     news_data = []
     for title, date, photo_url, detail_url in zip(titles, dates, photo_urls, detail_urls):
-        if 'ago' in date.text:
-            time_ago = date.text
-        else:
-            time_ago = None
-        
+
         resulltLink = ''
 
         if not detail_url['href'].startswith('http'):
             urlPrefix = 'https://www.investing.com'
             resulltLink = urlPrefix + detail_url['href']
-            
+
         news_item = {
             'title': title.text,
-            'timeAgo': time_ago,
+            'timeAgo': date.text,
             'photoUrl': photo_url['data-src'],
             'linkUrl': resulltLink
         }
@@ -48,6 +65,10 @@ def parse_news():
 
     # JSON verisini geri döndürüyoruz
     return json_data
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
